1474b5f940129b581749b2eae41d92102f5b303e
chingakhamronald <ronaldoch.lamzing@gmail.com>
2024-06-15 10:45:14 +0530

    file upload added

--- app/(layout)/assignments/_component/student-form.tsx
+++ app/(layout)/assignments/_component/student-form.tsx
@@ -1,18 +1,24 @@
-import { InputField } from "@/components/custom-field";
+"use client";
+
+import { FileUpload } from "@/app/(layout)/assignments/_component/file-upload";
 import { Button, Card } from "@nextui-org/react";
 import { Form, Formik } from "formik";
 import React from "react";
 
-export const StudentForm = () => {
+export const StudentForm = ({ subject }: { subject: string }) => {
   return (
-    <div>
+    <div className="mt-4">
       <Card className="p-6" shadow="sm">
-        <h1 className="text-3xl font-bold text-center mb-5">Assignment</h1>
+        <h1 className="text-3xl font-bold text-center mb-5">
+          Upload Assignment
+        </h1>
         <Formik
           enableReinitialize={true}
           // validationSchema={validationSchema}
           initialValues={{
-            semester: "",
+            subject: subject,
+            path: null,
+            projectName: "New",
           }}
           onSubmit={(e) => {
             console.log({ "e...": e });
@@ -23,7 +29,7 @@ export const StudentForm = () => {
           {({ setFieldValue, values }) => (
             <Form>
               <div className="w-full flex-wrap md:flex-nowrap">
-                <InputField id="semester" name="semester" label="Semester" />
+                <FileUpload setFieldValue={setFieldValue} name="path" />
               </div>
               <div className="text-center mt-5">
                 <Button type="submit">Submit</Button>

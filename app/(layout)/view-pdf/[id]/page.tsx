019ea57adc0b354bba885454d8e19588e9522b17
chingakhamronald <ronaldoch.lamzing@gmail.com>
2024-06-16 13:38:44 +0530

    view file on teacher

--- app/(layout)/view-pdf/[id]/page.tsx
+++ app/(layout)/view-pdf/[id]/page.tsx
@@ -5,15 +5,23 @@ import { PdfViewer } from "../../assignments/_component/pdf-viewer";
 import { useQueryUploadFileByStudent } from "@/hook/useQueryUploadFileByStudent";
 import { useSession } from "next-auth/react";
 import { useQueryUploadFileByTeacher } from "@/hook/useQueryUploadFileByTeacher";
+import { useGlobalStore } from "@/store/useStore";
 
-const ViewPdf = ({ params }: { params: { id: string } }) => {
+const ViewPdf = ({ params }: { params: any }) => {
   const { data: session } = useSession();
+
+  const { projectId } = useGlobalStore();
+
+  console.log({
+    "params...": params,
+    "projectId...": projectId,
+  });
 
   const { isLoadingViewFileByStudent, viewFileByStudent } =
     useQueryUploadFileByStudent(params.id, session?.user.id ?? "");
 
   const { viewFileByTeacher, isLoadingViewFileByTeacher } =
-    useQueryUploadFileByTeacher(params.id, session?.user.id ?? "");
+    useQueryUploadFileByTeacher(projectId, params.id);
 
   if (isLoadingViewFileByStudent || isLoadingViewFileByTeacher) {
     return <div>Loading...</div>;
--- hook/useQueryUploadFileByTeacher.ts
+++ hook/useQueryUploadFileByTeacher.ts
@@ -3,8 +3,11 @@ import axios from "axios";
 
 export const useQueryUploadFileByTeacher = (projectId: string, studentId: string) => {
   const { data: viewFileByTeacher, isLoading: isLoadingViewFileByTeacher } = useQuery({
+
+
     queryKey: ['UploadFileByTeacher'],
     queryFn: async () => {
+      console.log({ 'projectId...': projectId, 'studentId...': studentId });
       const res = await axios.get(`/api/docs/${projectId}/bystudentId/${studentId}`)
       console.log({ 'res...': res.data });
       return res.data

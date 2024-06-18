2237e92d75b721b119639bf4386b296cd3f3359e
RobsonWairokpam <robson23174@gmail.com>
2024-06-19 00:57:18 +0530

    upload project question doc

--- app/api/project/byTeacherId/[userId]/route.ts
+++ app/api/project/byTeacherId/[userId]/route.ts
@@ -1,6 +1,8 @@
 import { NextApiResponse } from 'next';
 import { NextRequest, NextResponse } from 'next/server';
 import { prisma } from '../../../../../config/prisma/db';
+import { S3 } from 'aws-sdk';
+import { ObjectCannedACL } from '@aws-sdk/client-s3';
 
 export async function GET(req: Request, { params }: { params: any }) {
   const { userId } = params;
@@ -22,21 +24,46 @@ export async function POST(
   { params }: { params: { userId: string } }
 ) {
   try {
-    const body = await req.json();
     const userId = params.userId;
 
-    const { question, semester, subject } = body;
+    const formData = await req.formData();
+    const file = formData.get('file') as File | null;
+    const question = formData.get('question') as string | null;
+    const semester = formData.get('semester') as string | null;
+    const subject = formData.get('subject') as string | null;
+
+    const type = file?.type;
+    if (!file) {
+      return NextResponse.json({ error: 'File not found' });
+    }
+    if (!type) {
+      return NextResponse.json({ error: 'FileType not found' });
+    }
+
+    const buffer = Buffer.from(await file.arrayBuffer());
+
+    console.log({
+      buffer________: buffer,
+      RES______: file,
+      question: question,
+      semester: semester,
+      subject: subject
+    });
+
     const teacher = await getTeacher(userId);
 
     if (!teacher) {
       return NextResponse.json({ error: 'Teacher not Found' });
     }
 
+    const res = await uploadFile(buffer, file.name, type);
+
     const createTeacher = await prisma.project.create({
       data: {
         question: question ?? '',
         semester: semester ?? '',
         subject: subject ?? '',
+        path: res.Location ?? '',
         teacherId: teacher.id
       }
     });
@@ -51,4 +78,18 @@ function getTeacher(id: string) {
   return prisma.teacher.findUnique({
     where: { userId: id }
   });
+}
+
+async function uploadFile(dataBuffer: Buffer, fileName: string, type: string) {
+  const s3 = new S3();
+  const uploadResultw = await s3
+    .upload({
+      Bucket: 'mitnewbucket',
+      Body: dataBuffer,
+      Key: `${fileName}-${Date.now()}`,
+      ACL: 'public-read' as ObjectCannedACL
+    })
+    .promise();
+
+  return uploadResultw;
 }
